4097a05a4a06d058270b9a20f1fa84820f662236
David Anusontarangkul <anusontarangkul.d@gmail.com>
2022-04-24 12:36:53 -0700

    set up database context

---
+++ src/context/DatabaseContext.js
@@ -0,0 +1,27 @@
+import React, { createContext, useState, useContext, useEffect } from 'react'
+
+const Database = createContext()
+
+const DatabaseContext = ({ children }) => {
+
+    const data = [
+        { name: 'TABLE_1', db_name: 'DEMO_DB', schema_name: 'PUBLIC', },
+        { name: 'TABLE_2', db_name: 'DEMO_DB', schema_name: 'INFORMATION_SCHEMA', },
+        { name: 'TABLE_3', db_name: 'SNOWFLAKE', schema_name: 'PUBLIC', },
+        { name: 'TABLE_4', db_name: 'SNOWFLAKE_SAMPLE_DATA', schema_name: 'INFORMATION_SCHEMA', },
+        { name: 'CALL_CENTER', db_name: 'SNOWFLAKE_SAMPLE_DATA', schema_name: 'TPCDS_SF100TCL', },
+        { name: 'CATALOG_PAGE', db_name: 'SNOWFLAKE_SAMPLE_DATA', schema_name: 'TPCDS_SF100TCL', }
+    ]
+
+    return (
+        <Database.Provider value={data}>
+            {children}
+        </Database.Provider>
+    )
+}
+
+export default DatabaseContext
+
+export const DatabaseState = () => {
+    return useContext(Database)
+}
