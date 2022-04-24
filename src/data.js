291dee250f22cbc73d2b003e415d7b282a56b444
David Anusontarangkul <anusontarangkul.d@gmail.com>
2022-04-24 09:12:59 -0700

    add skeleton of components

---
+++ src/data.js
@@ -0,0 +1,8 @@
+[
+    { name: 'TABLE_1', db_name: 'DEMO_DB', schema_name: 'PUBLIC', },
+    { name: 'TABLE_2', db_name: 'DEMO_DB', schema_name: 'INFORMATION_SCHEMA', },
+    { name: 'TABLE_3', db_name: 'SNOWFLAKE', schema_name: 'PUBLIC', },
+    { name: 'TABLE_4', db_name: 'SNOWFLAKE_SAMPLE_DATA', schema_name: 'INFORMATION_SCHEMA', },
+    { name: 'CALL_CENTER', db_name: 'SNOWFLAKE_SAMPLE_DATA', schema_name: 'TPCDS_SF100TCL', },
+    { name: 'CATALOG_PAGE', db_name: 'SNOWFLAKE_SAMPLE_DATA', schema_name: 'TPCDS_SF100TCL', }
+]
