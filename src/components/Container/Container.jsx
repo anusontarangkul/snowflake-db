2cf322b9139b39e87fcc72b85c343e7331244d85
David Anusontarangkul <anusontarangkul.d@gmail.com>
2022-04-25 20:17:36 -0700

    removed test

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -5,7 +5,9 @@ import NameContainer from '../NameContainer/NameContainer.jsx';
 const Container = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>SnowFlake Database</h1>
+      <h1 className={styles.title} data-testid='title'>
+        SnowFlake Database
+      </h1>
       <NameContainer />
     </div>
   );
