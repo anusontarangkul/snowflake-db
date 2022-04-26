981eb0ad18af22b43315b5cb8e486aeb102c6824
David Anusontarangkul <anusontarangkul.d@gmail.com>
2022-04-25 17:17:29 -0700

    render tables

--- src/components/SchemaName/SchemaName.jsx
+++ src/components/SchemaName/SchemaName.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
+import Table from '../Table/Table';
 
 const SchemaName = ({ schema }) => {
   const [table, setTable] = useState(false);
   const handleClick = (e) => {
-    setTable(true);
+    setTable(!table);
   };
-  return <p onClick={handleClick}>{schema.schema_name}</p>;
+  return (
+    <>
+      <div>
+        <p onClick={handleClick}>{schema.schema_name}</p>
+      </div>
+      {table && <Table schema={schema} />}
+    </>
+  );
 };
 
 export default SchemaName;
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DatabaseState } from '../../context/DatabaseContext';
 
-const Table = () => {
-  return <div></div>;
+const Table = ({ schema }) => {
+  const { data, openDB } = DatabaseState();
+  const [openTable, setOpenTable] = useState(false);
+
+  const findTables = data.filter((table) => {
+    return (
+      table.db_name === schema.db_name &&
+      table.schema_name === schema.schema_name
+    );
+  });
+
+  console.log('appr tables', findTables);
+
+  const handleClick = (e) => {
+    setOpenTable(!openTable);
+  };
+  return (
+    <div>
+      <p onClick={handleClick}>Table</p>
+      {openTable &&
+        findTables.map((table, i) => {
+          return <p key={i}>{table.name}</p>;
+        })}
+    </div>
+  );
 };
 
 export default Table;
